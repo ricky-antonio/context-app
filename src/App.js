23332116069922239dbfb50c31e1c55d9f143dc1
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-07-10 14:24:46 -0700

    Add sign in form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React, { Component } from 'react';
+import Form from './Form';
 import Navbar from './Navbar';
+
 
 function App() {
   return (
-
+    <>
     <Navbar />
+    <Form /> 
+    </>
   );
 }
 
